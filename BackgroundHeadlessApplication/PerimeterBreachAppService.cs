9f6ebffc3a0737f0c4bbc8623348986c23d58296
codingbandit <carey.payette@gmail.com>
2015-10-23 09:52:48 -0400

    Initial Commit

---
+++ BackgroundHeadlessApplication/PerimeterBreachAppService.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.Background;
+using Windows.Devices.Gpio;
+using Windows.Foundation.Collections;
+
+namespace BackgroundHeadlessApplication
+{
+    public sealed class PerimeterBreachAppService : IBackgroundTask
+    {
+        private BackgroundTaskDeferral _deferral;
+        private AppServiceConnection _connection;
+        private int _sensorPinNumber = 18;
+        private GpioPin _sensorPin;
+        private int _ledPinNumber = 23;
+        private GpioPin _ledPin;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            //keep this background task alive
+            _deferral = taskInstance.GetDeferral();
+            taskInstance.Canceled += OnTaskCanceled;
+
+            //execution triggered by another application requesting this App Service
+            //assigns an event handler to fire when a message is received from the client
+            var triggerDetails = taskInstance.TriggerDetails as AppServiceTriggerDetails;
+            _connection = triggerDetails.AppServiceConnection;
+            _connection.RequestReceived += Connection_RequestReceived;
+
+            //initialize sensor and led pins
+            GpioController controller = GpioController.GetDefault();
+            _sensorPin = controller.OpenPin(_sensorPinNumber);
+            _sensorPin.SetDriveMode(GpioPinDriveMode.InputPullUp); //high when door is open, low when closed
+            _sensorPin.ValueChanged += SensorPin_ValueChanged; //assigns event handler to fire when 
+            _ledPin = controller.OpenPin(_ledPinNumber);
+            _ledPin.SetDriveMode(GpioPinDriveMode.Output);
+
+            //send initial notification of the startup state of the sensor
+            //toggle current value so initial notification is made
+            NotifyClientsOfPerimeterState().Wait();
+        }
+
+        private async void SensorPin_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
+        {
+            await NotifyClientsOfPerimeterState();
+        }
+
+        private async Task NotifyClientsOfPerimeterState()
+        {
+            var _sensorCurrentValue = _sensorPin.Read();
+            var messages = new ValueSet(); //name value pair
+
+            if (_sensorCurrentValue == GpioPinValue.High)
+            {
+                //send perimeter breached
+                messages.Add("Perimeter Notification", "Breached");
+            }
+            else
+            {
+                //send perimeter secure
+                messages.Add("Perimeter Notification", "Secure");
+            }
+
+            //send message to the client
+            var response = await _connection.SendMessageAsync(messages);
+
+            if (response.Status == AppServiceResponseStatus.Success)
+            {
+                var result = response.Message["Response"];
+                //optionally log result from client
+            }
+        }
+
+
+        private async void Connection_RequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            // if you are doing anything awaitable, you need to get a deferral
+            var requestDeferral = args.GetDeferral();
+            var returnMessage = new ValueSet();
+            try
+            {
+                //obtain and react to the command passed in by the client
+                var message = args.Request.Message["Request"] as string;
+                switch (message)
+                {
+                    case "Turn LED On":
+                        _ledPin.Write(GpioPinValue.High);
+                        break;
+                    case "Turn LED Off":
+                        _ledPin.Write(GpioPinValue.Low);
+                        break;
+                }
+                returnMessage.Add("Response", "OK");
+            }
+            catch (Exception ex)
+            {
+                returnMessage.Add("Response", "Failed: " + ex.Message);
+            }
+
+            await args.Request.SendResponseAsync(returnMessage);
+ 
+            //let the OS know that the action is complete
+            requestDeferral.Complete();
+        }
+
+        private void OnTaskCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            if (_deferral != null)
+            {
+                _deferral.Complete();
+            }
+        }
+    }
+}
---
+++ HeadedApplication/MainPage.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Windows.ApplicationModel.AppService;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace HeadedApplication
+{
+    
+    public sealed partial class MainPage : Page
+    {
+        private AppServiceConnection _perimeterBreachService = null;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            SetupAppService();
+        }
+
+        private async void SetupAppService()
+        {
+            // find the installed application(s) that expose the app service PerimeterBreachService
+            var listing = await AppServiceCatalog.FindAppServiceProvidersAsync("PerimeterBreachService");
+            var packageName = "";
+            // there may be cases where other applications could expose the same App Service Name, in our case
+            // we only have the one
+            if (listing.Count == 1)
+            {
+                packageName = listing[0].PackageFamilyName;
+            }
+            _perimeterBreachService = new AppServiceConnection();
+            _perimeterBreachService.AppServiceName = "PerimeterBreachService";
+            _perimeterBreachService.PackageFamilyName = packageName;
+            //open app service connection
+            var status = await _perimeterBreachService.OpenAsync();
+
+            if (status != AppServiceConnectionStatus.Success)
+            {
+                //something went wrong
+                txtStatus.Text = "Could not connect to the App Service: " + status.ToString();
+            }
+            else
+            {
+                //add handler to receive app service messages (Perimiter messages)
+                _perimeterBreachService.RequestReceived += PerimeterBreachService_RequestReceived;
+            }
+        }
+
+        private async void PerimeterBreachService_RequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () => 
+            {
+                // if you are doing anything awaitable, you need to get a deferral
+                var requestDeferral = args.GetDeferral();
+                var returnMessage = new ValueSet();
+                var uiMessage = "";
+                var uiMessageBrush = new SolidColorBrush(Colors.Black);
+
+                try
+                {
+                    //determine message sent from the background task (perimeter notifications)
+                    var message = args.Request.Message["Perimeter Notification"] as string;
+                    switch (message)
+                    {
+                        case "Breached":
+                            uiMessage = "Perimeter Breached!!! Alert!!!";
+                            uiMessageBrush = new SolidColorBrush(Colors.Red);
+                            break;
+                        case "Secure":
+                            uiMessage = "Perimeter Secured... Move along...";
+                            break;
+                    }
+                    returnMessage.Add("Response", "OK");
+                }
+                catch (Exception ex)
+                {
+                    returnMessage.Add("Response", "Failed: " + ex.Message);
+                }
+
+                //return OK or Failed response back to the background task
+                await args.Request.SendResponseAsync(returnMessage);
+
+
+                //let the OS know that the action is complete
+                requestDeferral.Complete();
+
+                txtStatus.Text = uiMessage;
+                txtStatus.Foreground = uiMessageBrush;
+            });
+        }
+
+        private async void btnCommand_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            string command = "";
+            var message = new ValueSet();
+            var statusColor = new SolidColorBrush(Colors.Black);
+            switch(btn.Name)
+            {
+                case "btnLEDOn":
+                    command = "Turn LED On";
+                    break;
+                case "btnLEDOff":
+                    command = "Turn LED Off";
+                    break;
+            }
+            message.Add("Request", command);
+
+            //use the app service connection to send LED Command (based on button pressed)
+            var response = await _perimeterBreachService.SendMessageAsync(message);
+            var result = "";
+            //analyze response from the background task
+            if(response.Status == AppServiceResponseStatus.Success)
+            {
+                result = response.Message["Response"] as string;
+            }
+            else
+            {
+                result = "Something went wrong: " + response.Status;
+                statusColor = new SolidColorBrush(Colors.Red);
+            }
+
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                txtStatus.Text = result;
+                txtStatus.Foreground = statusColor;
+            });
+        }
+    }
+}
